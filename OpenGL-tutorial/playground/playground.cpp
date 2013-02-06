69ad63f3460f07f61b1028e08a3cfd1c3a114b8b
Jijidici <jdefaye.jijidici@gmail.com>
2013-02-06 18:48:32 +0100

    Add the initialization of a window (NOT FINISH)

--- OpenGL-tutorial/playground/playground.cpp
+++ OpenGL-tutorial/playground/playground.cpp
@@ -1,63 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <iostream>
+#include <cstdlib>
 #include <GL/glew.h>
-
 #include <GL/glfw.h>
-
 #include <glm/glm.hpp>
-using namespace glm;
 
-int main( void )
-{
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		return -1;
+static const size_t WINDOW_WIDTH = 800;
+static const size_t WINDOW_HEIGHT = 600;
+static const size_t BYTES_BY_SAMPLE = 32;
+
+int main(int argc, char* argv){
+	//Initialize the GLFW
+	if(!glfwInit()){
+		std::cout<<"[!]-> ERROR while initializing GLFW."<<std::endl;
+		return EXIT_FAILURE;
 	}
-
-	glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
-	glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE,GL_TRUE);
+	
+	//setting the GLFW config states
+	glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4); //Anti-aliasing
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
-	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Open a window and create its OpenGL context
-	if( !glfwOpenWindow( 1024, 768, 0,0,0,0, 32,0, GLFW_WINDOW ) )
-	{
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //inhibit opengl 2
+	
+	//Open a window and intialize its OpenGL context
+	if(!glfwOpenWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0, 0, 0, BYTES_BY_SAMPLE, 0, GLFW_WINDOW)){
+		std::cout<<"[!]-> ERROR while opening a GLFW window."<<std::endl;
 		glfwTerminate();
-		return -1;
+		return EXIT_FAILURE;
 	}
-
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		return -1;
-	}
-
-	glfwSetWindowTitle( "Playground" );
-
-	// Ensure we can capture the escape key being pressed below
-	glfwEnable( GLFW_STICKY_KEYS );
-
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	do{
-		// Draw nothing, see you in tutorial 2 !
-
-		// Swap buffers
-		glfwSwapBuffers();
-
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
-		   glfwGetWindowParam( GLFW_OPENED ) );
-
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
-
-	return 0;
+	
+	return EXIT_SUCCESS;
 }
 
